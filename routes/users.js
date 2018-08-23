6836cdb44b2bf6644087828b85f6dd52ced3fa9d
dedmakap <mylnikovdma@gmail.com>
2018-08-23 11:05:31 +0300

    Users page ready

--- routes/users.js
+++ routes/users.js
@@ -3,26 +3,27 @@ var router = express.Router();
 var jwt = require('jsonwebtoken');
 var User = require('../database/user');
 
-function middle(req, res, next) {
+function checkAdminRole(req, res, next) {
   if (!req.cookies.token) {
     res.redirect('/');
   }
-  var email = jwt.verify(req.cookies.token, 'secret'); 
+  var email = jwt.verify(req.cookies.token, 'secret');
   User.findOne({ email: email }, function (err, data) {
     if (err) return console.log(err);
-    var role = data.role;
+    var { role } = data;
     if (role === 'admin') {
-      next();
+      return next();
     }
-    else {
-      res.redirect('/');
-    }
-})
+    return res.redirect('/');
+  })
 }
 
 /* GET users listing. */
-router.get('/', middle, function(req, res, next) {
-  res.send('boi');
+router.get('/', checkAdminRole, function (req, res, next) {
+  User.find({}, function (err, userslist) {
+    res.render('users', { people: userslist, title: 'Users list page', css: ['bootstrap.min.css'] })
+  })
+
 });
 
 module.exports = router;
