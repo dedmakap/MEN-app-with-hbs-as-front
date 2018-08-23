fb728b847acc3c11ae73740300e7b40613600bd5
dedmakap <mylnikovdma@gmail.com>
2018-08-23 15:41:30 +0300

    User's profile page reworked using middleware

--- routes/userpage.js
+++ routes/userpage.js
@@ -3,34 +3,35 @@ var router = express.Router();
 var User = require('../database/user');
 var jwt = require('jsonwebtoken');
 
-
-router.get('/:id', function(req, res, next) {
-  var token = req.cookies.token;
-  if (!token) {
-    res.redirect('/')
+function checkGuestRole(req, res, next) {
+  if (!req.cookies.token) {
+    res.redirect('/');
   }
   var id = req.params['id'];
-  var email = jwt.verify(token, 'secret'); 
+  var email = jwt.verify(req.cookies.token, 'secret');
   User.findOne({ email: email }, function (err, guest) {
     if (err) throw err
     if ((guest.role !== 'admin') && (guest._id != id)) {
       console.log(typeof guest._id, typeof id);
       res.redirect('/')
-    }  
-    else {
-      User.findOne({_id: id}, function(err,owner) {
-        res.render('userpage', { 
-          firstname: owner.firstName, 
-          email: owner.email,
-          username: owner.userName,
-          role: owner.role,
-          css:['userpage.css']});
-      })
     }
+    else next();
+  })
+}
 
-    
-  });
-  
+router.get('/:id', checkGuestRole, function (req, res, next) {
+  var id = req.params['id'];
+  User.findOne({ _id: id }, function (err, owner) {
+    res.render('userpage', {
+      title: "User's profile page",
+      firstname: owner.firstName,
+      email: owner.email,
+      username: owner.userName,
+      role: owner.role,
+      css: ['userpage.css']
+    });
+  })
+
 });
 
 module.exports = router;
