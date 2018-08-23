5522b37f21d21b4a32d0278ecd22a2a395260015
dedmakap <mylnikovdma@gmail.com>
2018-08-23 17:18:59 +0300

    User's profile page| added avatar

--- routes/userpage.js
+++ routes/userpage.js
@@ -2,6 +2,11 @@ var express = require('express');
 var router = express.Router();
 var User = require('../database/user');
 var jwt = require('jsonwebtoken');
+var multer = require('multer');
+var path = require("path");
+var fs = require("fs");
+
+var upload = multer({dest: './public/images'});
 
 function checkGuestRole(req, res, next) {
   if (!req.cookies.token) {
@@ -12,7 +17,6 @@ function checkGuestRole(req, res, next) {
   User.findOne({ email: email }, function (err, guest) {
     if (err) throw err
     if ((guest.role !== 'admin') && (guest._id != id)) {
-      console.log(typeof guest._id, typeof id);
       res.redirect('/')
     }
     else next();
@@ -28,10 +32,34 @@ router.get('/:id', checkGuestRole, function (req, res, next) {
       email: owner.email,
       username: owner.userName,
       role: owner.role,
+      id: owner._id,
       css: ['userpage.css']
     });
   })
 
 });
+
+router.post('/:id',  upload.single('file'), function (req, res, next) {
+  //console.log(req);
+  const tempPath = req.file.path;
+  const targetPath = path.join(__dirname, "../public/images/" + req.params['id'] +".png");
+
+  if (path.extname(req.file.originalname).toLowerCase() === ".png") {
+    fs.rename(tempPath, targetPath, err => {
+      if (err) throw (err)
+
+      res.redirect('/users/'+req.params['id']);
+      });
+    } else {
+      fs.unlink(tempPath, err => {
+        if (err) throw (err);
+
+        res
+          .status(403)
+          .contentType("text/plain")
+          .end("Only .png files are allowed!");
+      });
+    } 
+})
 
 module.exports = router;
