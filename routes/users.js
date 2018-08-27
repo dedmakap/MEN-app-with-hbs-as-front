2e9d46617c365b1ae3913ecf9fbd5d9d1ae132cd
dedmakap <mylnikovdma@gmail.com>
2018-08-27 17:56:52 +0300

    Users search features; Add search by name

--- routes/users.js
+++ routes/users.js
@@ -3,6 +3,20 @@ var router = express.Router();
 var jwt = require('jsonwebtoken');
 var User = require('../database/user');
 
+function generateAgeRange() {
+  var ageFrom = 10;
+  var ageTo = 70;
+  var ageRange = [];
+  for (let i = 0; i <= ageTo-10; i++) {
+    ageRange[i] = ageFrom
+    ageFrom++
+  }
+  return ageRange;
+}
+
+
+
+
 function checkAdminRole(req, res, next) {
   if (!req.cookies.token) {
     res.redirect('/');
@@ -18,10 +32,19 @@ function checkAdminRole(req, res, next) {
   })
 }
 
-/* GET users listing. */
+router.get('/search/', function (req, res, next) {
+  var searchKey = new RegExp(req.query.q.trim(), 'i');
+  console.log(searchKey);
+  User.find({ firstName: searchKey }, function (err, userslist) {
+    console.log(userslist);
+    res.render('users', { people: userslist, title: 'Users list page', css: ['bootstrap.min.css', 'users.css'] })
+  })
+
+})
+
 router.get('/', checkAdminRole, function (req, res, next) {
   User.find({}, function (err, userslist) {
-    res.render('users', { people: userslist, title: 'Users list page', css: ['bootstrap.min.css'] })
+    res.render('users', { ageRange: generateAgeRange(), people: userslist, title: 'Users list page', css: ['bootstrap.min.css', 'users.css'] })
   })
 
 });
